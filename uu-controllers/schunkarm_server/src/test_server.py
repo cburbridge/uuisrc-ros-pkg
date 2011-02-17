fe7fe4560c5aa2915dd9d3cf6c6744f015c2a36c
Lorenzo Riano <lorenzo.riano@gmail.com>
2011-02-17 16:01:50 +0000

    The same as before

--- uu-controllers/schunkarm_server/src/test_server.py
+++ uu-controllers/schunkarm_server/src/test_server.py
@@ -5,12 +5,30 @@ import rospy
 
 import actionlib
 import schunkarm_server.msg
+import random
 
 def main():
     client =  actionlib.SimpleActionClient("schunkarm_server", schunkarm_server.msg.movearmAction)
     client.wait_for_server()
     
     goal = schunkarm_server.msg.movearmGoal()
+    goal.goal_pose.header.stamp = rospy.Time.now()
+    goal.goal_pose.header.frame_id = "/schunk/position/PAM112_BaseConector"
+    
+    goal.goal_pose.pose.position.x = -0.51
+    goal.goal_pose.pose.position.y = -0.000501
+#    goal.goal_pose.pose.position.z = 0.6252
+
+#    goal.goal_pose.pose.position.x = random.uniform(0,0.5)
+#    goal.goal_pose.pose.position.y = random.uniform(-0.1,0.1)
+    goal.goal_pose.pose.position.z = random.uniform(0.3,0.8)
+    
+    goal.goal_pose.pose.orientation.x = -0.000236
+    goal.goal_pose.pose.orientation.y = 0.73538
+    goal.goal_pose.pose.orientation.z = 0.000226
+    goal.goal_pose.pose.orientation.w = -0.677654
+      
+    
     print "Goal: ", goal
     client.send_goal(goal)
     
