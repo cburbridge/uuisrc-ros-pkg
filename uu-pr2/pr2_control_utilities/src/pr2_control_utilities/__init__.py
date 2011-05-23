0b22f29d2738455c507fbcc6494815b2f5ebd179
Lorenzo Riano <lorenzo.riano@gmail.com>
2011-05-23 15:17:59 +0100

    Deleted the uu-pr2

--- uu-pr2/pr2_control_utilities/src/pr2_control_utilities/__init__.py
+++
@@ -1,9 +0,0 @@
-import roslib
-roslib.load_manifest("pr2_control_utilities")
-
-from pr2_joint_mover import PR2JointMover
-from pr2_joint_mover import RobotState
-from kinematics_loader import KinematicsLoader 
-
-from interpolated_ik_motion_planner.ik_utilities import IKUtilities
-from pr2_planning import PR2MoveArm
